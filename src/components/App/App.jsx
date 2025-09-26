db27ee6e4832dcaa5031c87d53f08557f21b4018
Bondarev Artem <bond210484@mail.ru>
2025-09-26 09:52:58 +0700

    error fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
-import { Card } from "../Card/Card";
 import { ImageCard } from "../ImageCard/ImageCard";
 
 
-export function App(props) {
+export function App() {
+    const servData = [
+        {img:'https://img.icons8.com/?size=100&id=0mRGdD9mF99z&format=png&color=000000', title:'title text', text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"},
+    {title:'title text', text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"}]
     return(
         <div>
-        {<ImageCard />}
-        {<Card/>}
+            {servData.map((item, index) => (
+                <ImageCard key={index} {...item}/>
+            ))}
         </div>      
     )
 }
